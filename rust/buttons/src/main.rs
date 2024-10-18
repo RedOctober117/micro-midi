2e1d573d192abcb887028088128fee9ba25a729b
RedOctober117 <dbcahilly3@gmail.com>
2024-10-17 22:38:06 -0500

    Update main.rs

--- rust/buttons/src/main.rs
+++ rust/buttons/src/main.rs
@@ -8,13 +8,15 @@ use panic_halt as _;
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    // if you need adc, https://github.com/Rahix/avr-hal/blob/main/examples/arduino-leonardo/src/bin/leonardo-adc.rs
 
     let num_rows: u8 = 2;
     let num_cols: u8 = 2;
     let mut current_row: u8 = 0;
 
-    // row: high
-    // col: low
+    // rows are initialized to output and then set to high, their default
+    // state.
+    // cols are initialized to a pullup state, their default state.
     let mut rows: Vec<_, 2> = Vec::new();
     let _ = rows.push(pins.a0.into_output().downgrade());
     let _ = rows.push(pins.a1.into_output().downgrade());
@@ -27,6 +29,7 @@ fn main() -> ! {
     let _ = cols.push(pins.a2.into_pull_up_input().downgrade());
     let _ = cols.push(pins.a3.into_pull_up_input().downgrade());
 
+    // the four test leds, one for each button, top to bottom, left to right.
     let mut leds: Vec<_, 4> = Vec::new();
     let _ = leds.push(pins.d4.into_output().downgrade());
     let _ = leds.push(pins.d5.into_output().downgrade());
@@ -34,10 +37,14 @@ fn main() -> ! {
     let _ = leds.push(pins.d7.into_output().downgrade());
 
     loop {
+        // set all leds to low. this effect is so fast to the human eye that
+        // nothing visibily changes even if a button is still depressed.
         for led in &mut leds {
             led.set_low();
         }
 
+        // set the current row to low, and see what columns get pulled down with it.
+        // https://www.dribin.org/dave/keyboard/one_html/
         rows[current_row as usize].set_low();
         for i in 0..num_cols {
             if let Some(pin) = cols.get(i as usize) {
